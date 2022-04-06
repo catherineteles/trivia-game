c6f0c8fb07a24576f325915802c51588b218e9e1
cathe <cacateles12@gmail.com>
2022-04-06 19:22:07 -0300

    [R12 e 13] update

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
+    const minScore = 3;
+    const { savedScore } = this.props;
     return (
       <div>
         <Header />
-        <p>Adelson vai arrasar no THUNK :rocket:</p>
+        { savedScore < minScore ? (
+          <h2 data-testid="feedback-text">Could be better...</h2>)
+          : (<h2 data-testid="feedback-text">Well Done!</h2>)}
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  savedScore: state.player.score,
+});
+
+Feedback.propTypes = {
+  savedScore: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
