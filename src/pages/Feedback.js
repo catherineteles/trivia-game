bab64113d1f69bb904d0e2c358ebfbb325ff1781
cathe <cacateles12@gmail.com>
2022-04-06 09:40:15 -0300

    requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
-class App extends React.Component {
+class Feedback extends React.Component {
   render() {
     return (
       <div>
         <Header />
         <p>Adelson vai arrasar no THUNK :rocket:</p>
-        <Link to="/feedback">Feedback</Link>
       </div>
     );
   }
 }
 
-export default App;
+export default Feedback;
