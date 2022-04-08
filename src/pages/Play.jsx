f695cc0c692d8ccd974b9eb9705421676e636bf6
Adolligit <adelson.onod@gmail.com>
2022-04-07 21:29:55 -0300

    [R18] O usuário pode voltar para a página 'Home' a parti da 'Feedback'

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -11,6 +11,7 @@ class Play extends React.Component {
     this.state = {
       index: 0,
       clock: 30,
+      timerFreaze: 0,
       showNext: false,
       answered: false,
     };
@@ -59,7 +60,7 @@ class Play extends React.Component {
 
   render() {
     const { results } = this.props;
-    const { index, answered, showNext, clock } = this.state;
+    const { index, answered, showNext, clock, timerFreaze } = this.state;
 
     return (
       <div>
@@ -68,6 +69,7 @@ class Play extends React.Component {
           showNextBtn={ this.showNextBtn }
           answered={ answered }
           result={ results[index] }
+          timer={ timerFreaze }
         />}
         { showNext && (
           <button
--- src/pages/Rank.js
+++ src/pages/Rank.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rank extends React.Component {
   render() {
+    const { history } = this.props;
+
     return (
       <div>
         <h1 data-testid="ranking-title">Rank</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Home
+        </button>
       </div>
     );
   }
 }
+
+Rank.propTypes = {
+  history: PropTypes.objectOf(),
+}.isRequired;
 
 export default Rank;
