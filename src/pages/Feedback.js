4212f5e5e5dd372f21831af4314536d115b13550
cathe <cacateles12@gmail.com>
2022-04-11 10:37:33 -0300

    added images to feedback page

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import wellDone from '../images/wellDone.svg';
+import doBetter from '../images/doBetter.svg';
 
 class Feedback extends React.Component {
   render() {
@@ -11,11 +13,29 @@ class Feedback extends React.Component {
       <div>
         <Header />
         <main>
-          <h2 data-testid="feedback-total-score">{savedScore}</h2>
-          <h2 data-testid="feedback-total-question">{savedAssertion}</h2>
+          <h2>
+            Your total score is:
+            <span data-testid="feedback-total-score">
+              {savedScore}
+            </span>
+          </h2>
+          <h2>
+            You answered
+            <span data-testid="feedback-total-question">
+              {savedAssertion}
+            </span>
+            questions correct
+          </h2>
           {savedAssertion < minPoints ? (
-            <h3 data-testid="feedback-text">Could be better...</h3>)
-            : <h3 data-testid="feedback-text">Well Done!</h3>}
+            <>
+              <img src={ doBetter } alt="Sad face" />
+              <h3 data-testid="feedback-text">Could be better...</h3>
+            </>)
+            : (
+              <>
+                <img src={ wellDone } alt="Well Done" />
+                <h3 data-testid="feedback-text">Well Done!</h3>
+              </>)}
           <button
             type="button"
             data-testid="btn-play-again"
