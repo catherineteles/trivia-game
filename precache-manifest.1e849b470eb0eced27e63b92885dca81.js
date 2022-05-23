90bcbfa7ef33d4b685f2cd41f8bdcfddcd8af615
cathe <cacateles12@gmail.com>
2022-05-23 16:46:37 -0300

    Updates

---
+++ precache-manifest.1e849b470eb0eced27e63b92885dca81.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b97eed1806ea762aa4d296869ed4dc7",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "0907c11da63fc748b524",
+    "url": "/trivia-game/static/css/main.5fdb6058.chunk.css"
+  },
+  {
+    "revision": "41b4cfe8a52ef09c0302",
+    "url": "/trivia-game/static/js/2.849cc7e1.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia-game/static/js/2.849cc7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0907c11da63fc748b524",
+    "url": "/trivia-game/static/js/main.19cc5b5b.chunk.js"
+  },
+  {
+    "revision": "b0c6c90c513f51408a18",
+    "url": "/trivia-game/static/js/runtime-main.4bc9de50.js"
+  },
+  {
+    "revision": "549fd09b5d15b2282eb7400b75a314ac",
+    "url": "/trivia-game/static/media/Animals.549fd09b.svg"
+  },
+  {
+    "revision": "641abe858ecbde774b4b9a42c131e2bc",
+    "url": "/trivia-game/static/media/Art.641abe85.svg"
+  },
+  {
+    "revision": "19d2ab8f180daa4ca5d3193b2f65b958",
+    "url": "/trivia-game/static/media/Entertainment: Board Games.19d2ab8f.svg"
+  },
+  {
+    "revision": "97b9a0a4306e478641b5d314a7ba431c",
+    "url": "/trivia-game/static/media/Entertainment: Books.97b9a0a4.svg"
+  },
+  {
+    "revision": "aafbbb4303d60271d9ca0a454abc68e5",
+    "url": "/trivia-game/static/media/Entertainment: Film.aafbbb43.svg"
+  },
+  {
+    "revision": "ac21f461c7d55ffadd867f385473fd29",
+    "url": "/trivia-game/static/media/Entertainment: Japanese Anime & Manga.ac21f461.svg"
+  },
+  {
+    "revision": "12c503aefdbe7c434a341ebf3be8ada5",
+    "url": "/trivia-game/static/media/Entertainment: Music.12c503ae.svg"
+  },
+  {
+    "revision": "5726322dd6be2a56da4c57c527f7825a",
+    "url": "/trivia-game/static/media/Entertainment: Video Games.5726322d.svg"
+  },
+  {
+    "revision": "06893475711487b6356693fccb469eaa",
+    "url": "/trivia-game/static/media/General Knowledge.06893475.svg"
+  },
+  {
+    "revision": "39a67f4cb6d7ebea4168518d404228ea",
+    "url": "/trivia-game/static/media/Geography.39a67f4c.svg"
+  },
+  {
+    "revision": "125fcb1d82297bc618a7988c8bdd4bef",
+    "url": "/trivia-game/static/media/History.125fcb1d.svg"
+  },
+  {
+    "revision": "80be2517117416cbccf7613836a4868b",
+    "url": "/trivia-game/static/media/Mythology.80be2517.svg"
+  },
+  {
+    "revision": "051b033d6e4c61726ce7b794eb8e4d4a",
+    "url": "/trivia-game/static/media/Politics.051b033d.svg"
+  },
+  {
+    "revision": "6dd8cf2c3ef2634d12e08473e13c7053",
+    "url": "/trivia-game/static/media/Science & Nature.6dd8cf2c.svg"
+  },
+  {
+    "revision": "0762b505a7f8a04693445e9cac29e060",
+    "url": "/trivia-game/static/media/Science: Computers.0762b505.svg"
+  },
+  {
+    "revision": "10063cfa85f5ef0e205058b44d32caa3",
+    "url": "/trivia-game/static/media/Science: Mathematics.10063cfa.svg"
+  },
+  {
+    "revision": "a6a1bfd1d692055e46b574e11918f522",
+    "url": "/trivia-game/static/media/Sports.a6a1bfd1.svg"
+  },
+  {
+    "revision": "bb59106706c3d303d86f86e5db0b925b",
+    "url": "/trivia-game/static/media/Vehicles.bb591067.svg"
+  },
+  {
+    "revision": "88e8e369ef657d277d172c77d7479f3c",
+    "url": "/trivia-game/static/media/doBetter.88e8e369.svg"
+  },
+  {
+    "revision": "5af2517c2eb53eac5c281c0560bfe8d7",
+    "url": "/trivia-game/static/media/wellDone.5af2517c.svg"
+  }
+]);
